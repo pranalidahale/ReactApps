83227fd2a6be08110c204b20b3594bad7f460e8f
Pranali Dahale <35517893+pranalidahale@users.noreply.github.com>
2022-09-06 19:22:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Expenses from "./Components/Expenses";
+function App() {
+  return (
+    <div className="App">
+      <h2>Expense Tracker</h2>
+      <Expenses></Expenses>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Expenses.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Expenses = (props) => {
+  return (
+    <div>
+      <span>28th Aug 2022</span>
+      <span>Milk</span>
+      <span>$5</span>
+    </div>
+  );
+};
+
+export default Expenses;
